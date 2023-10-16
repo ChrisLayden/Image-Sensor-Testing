8576d8177355d9981d13446758cb8a5b2dbda4b7
Christopher Layden <clayden7@mit.edu>
2023-10-16 11:23:53 -0400

    Modifying QE File

--- quantum_efficiency.py
+++ quantum_efficiency.py
@@ -19,25 +19,25 @@ print(np.mean(first_img), np.mean(second_img))
 year = 1900
 month = 1
 day = 1
-start_time = datetime.datetime(year=year, month=month, day=day, hour=12, minute=36, second=0, microsecond=1)
+stop_time = datetime.datetime(year=year, month=month, day=day, hour=12, minute=34, second=36, microsecond=1)
 exposure_time = 60
-end_time = start_time + datetime.timedelta(seconds=exposure_time)
+start_time = stop_time - datetime.timedelta(seconds=exposure_time)
 # Load csv using pandas. First column is a time.
 df = pd.read_csv('/Users/layden/Documents/TESS/QE/qe_visible.csv')
 df.columns = ['time', 'ThorLabs PD', 'Photodetector']
 df['time'] = df['time'].apply(lambda x: datetime.datetime.strptime(x, '%H:%M:%S.%f'))
 
-# Plot PD and photodetector reading vs time to check timing of light/dark images
-fig, ax1 = plt.subplots()
-ax1.set_xlabel('Time')
-ax1.set_ylabel('ThorLabs PD (W)')
-ax1.scatter(df['time'], df['ThorLabs PD'], s=1)
-ax2 = ax1.twinx()
-ax2.set_ylabel('Photodetector')
-ax2.scatter(df['time'], df['Photodetector'], s=1, color='red')
-fig.tight_layout()
-plt.show()
+# # Plot PD and photodetector reading vs time to check timing of light/dark images
+# fig, ax1 = plt.subplots()
+# ax1.set_xlabel('Time')
+# ax1.set_ylabel('ThorLabs PD (W)')
+# ax1.scatter(df['time'], df['ThorLabs PD'], s=1)
+# ax2 = ax1.twinx()
+# ax2.set_ylabel('Photodetector')
+# ax2.scatter(df['time'], df['Photodetector'], s=1, color='red')
+# fig.tight_layout()
+# plt.show()
 
 # Get entries that are greater than start_time and less than end_time
-df = df[(df['time'] > start_time) & (df['time'] < end_time)]
+df = df[(df['time'] > start_time) & (df['time'] < stop_time)]
 print(df['ThorLabs PD'].mean())
